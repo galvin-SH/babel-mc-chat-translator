283159c9d5b6de6717605cc3ae254afdf7a331f5
galvin-SH <mgalvin1993@gmail.com>
2023-12-18 03:29:47 -0800

    imported mod template code

---
+++ src/client/java/name/mcbabel/BabelClient.java
@@ -0,0 +1,10 @@
+package name.mcbabel;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BabelClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
